d567e5958c2e29c2bb6a94afdb4bc4011e8f8a4e
hishamkhirllah <145259864+hishamkhirllah@users.noreply.github.com>
2023-09-25 19:27:31 +0300

    colors list view part2

--- lib/views/widgets/colors_list_view.dart
+++ lib/views/widgets/colors_list_view.dart
@@ -1,29 +1,67 @@
 import 'package:flutter/material.dart';
 
-class ColorList extends StatelessWidget {
-  const ColorList({super.key});
-
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key, required this.isActive, required this.color});
+  final bool isActive;
+  final Color color;
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.blue,
-      radius: 34,
-    );
+    return isActive
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 38,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 36,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 38,
+          );
   }
 }
 
-class ColorListView extends StatelessWidget {
+class ColorListView extends StatefulWidget {
   const ColorListView({super.key});
 
   @override
+  State<ColorListView> createState() => _ColorListViewState();
+}
+
+class _ColorListViewState extends State<ColorListView> {
+  int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xFFA4243B),
+    Color(0xFFd8c99b),
+    Color(0xFF90e0ef),
+    Color(0xFFBD632F),
+    Color(0xFFe07a5f),
+    Color(0xFFf48c06),
+    Color(0xFF8cb369),
+    Color(0xFFfffcf2),
+  ];
+  @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 34 * 2,
+      height: 38 * 2,
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: colors.length,
         scrollDirection: Axis.horizontal,
         itemBuilder: (context, index) {
-          return const ColorList();
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 6),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
         },
       ),
     );
